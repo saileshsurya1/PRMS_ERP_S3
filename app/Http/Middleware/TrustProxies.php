65dae7c7e775a468e1ee888a3b504c10d7e3b1d0
Sailesh Dakua <156662652+saileshsurya1@users.noreply.github.com>
2026-08-28 18:37:10 +0530

    Set trusted proxies to allow all

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // 👈 CHANGE THIS LINE TO AN ASTERISK HERE
 
     /**
      * The headers that should be used to detect proxies.
